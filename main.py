753d25629dba29b8b6e9328dcd2be3777fc6ebf5
Joe Regan <joe.regan285@gmail.com>
2016-08-18 20:03:17 -0500

    Added todos 1 and 2

--- main.py
+++ main.py
@@ -1,32 +1,105 @@
 import webapp2
-import random
+
+
+# html boilerplate for the top of every page
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FlickList</title>
+</head>
+<body>
+    <h1>FlickList</h1>
+"""
+
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
 
 class Index(webapp2.RequestHandler):
-
-    def getRandomMovie(self):
-
-        # TODO: make a list with at least 5 movie titles
-
-        movlist = ('End of Evangelion', 'Akira', 'Lupin III: The Castle of Cagliostro', 'Jin-Roh', 'Ghost in the Shell')
-
-        # TODO: randomly choose one of the movies, and return it
-
-        return random.choice(movlist)
+    """ Handles requests coming in to '/' (the root of our site)
+        e.g. www.flicklist.com/
+    """
 
     def get(self):
-        movie = self.getRandomMovie()
-        tmovie = self.getRandomMovie()
-        while movie == tmovie:
-            tmovie = self.getRandomMovie()
 
-        # build the response string
-        response = "<h1>Movie of the Day</h1>"
-        response += "<ul><li>" + movie + "</li></ul>"
+        edit_header = "<h3>Edit My Watchlist</h3>"
 
-        response += "<h2>Tomorrow's Movie</h2>"
-        response += "<ul><li>" + tmovie + "</li></ul>"
+        # a form for adding new movies
+        add_form = """
+        <form action="/add" method="post">
+            <label>
+                I want to add
+                <input type="text" name="new-movie"/>
+                to my watchlist.
+            </label>
+            <input type="submit" value="Add It"/>
+        </form>
+        """
+
+        # TODO 1
+        # Include another form so the user can "cross off" a movie from their list.
+        remove_form = """
+        <form action="/remove" method="post">
+            <label>
+                I want to remove
+                <input type="text" name="removie"/>
+                from my watchlist.
+            </label>
+            <input type="submit" value="Remove it"/>
+        </form>
+        """
+
+
+        # TODO 4 (Extra Credit)
+        # modify your form to use a dropdown (<select>) instead a
+        # text box (<input type="text"/>)
+
+
+        response = page_header + edit_header + add_form + remove_form + page_footer
         self.response.write(response)
 
+
+class AddMovie(webapp2.RequestHandler):
+    """ Handles requests coming in to '/add'
+        e.g. www.flicklist.com/add
+    """
+
+    def post(self):
+        # look inside the request to figure out what the user typed
+        new_movie = self.request.get("new-movie")
+
+        # build response content
+        new_movie_element = "<strong>" + new_movie + "</strong>"
+        sentence = new_movie_element + " has been added to your Watchlist!"
+
+        response = page_header + "<p>" + sentence + "</p>" + page_footer
+        self.response.write(response)
+
+
+# TODO 2
+# Create a new RequestHandler class called CrossOffMovie, to receive and
+# handle the request from your 'cross-off' form. The user should see a message like:
+# "Star Wars has been crossed off your watchlist".
+class CrossOffMovie(webapp2.RequestHandler):
+
+    def post(self):
+        removie = self.request.get("removie")
+
+        removie_element = "<strong>" + removie + "</strong>"
+        sentence = removie_element + " has been removed from your Watchlist!"
+
+        response = page_header + "<p>" + sentence + "</p>" + page_footer
+        self.response.write(response)
+
+
+
+# TODO 3
+# Include a route for your cross-off handler, by adding another tuple to the list below.
 app = webapp2.WSGIApplication([
-    ('/', Index)
+    ('/', Index),
+    ('/add', AddMovie),
+    ('/remove', CrossOffMovie)
 ], debug=True)
